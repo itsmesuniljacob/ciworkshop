a33dc706a2e76c4b86f7368cb5c5a3e38402385a
Sunil.Jacob <sunil1405@gmail.com>
2018-09-05 14:15:55 +0530

    Linted

--- setup-scripts/Set-GitConfiguration.ps1
+++ setup-scripts/Set-GitConfiguration.ps1
@@ -7,22 +7,18 @@ param([String]$email)
 function isSuccess {
 	param([String]$property = "unknown")
 	if($?) {
-		Write-Host "Config $property... is updated"
+		"Config $property... is updated"
 	}
 	else {
-		Write-Host "Config $property... is not updated"
-	}	
+		"Config $property... is not updated"
+	}
 }
 
 git config --global user.name "$name"
-isSuccess -property user.name	
+isSuccess -property user.name
 git config --global user.email "$email"
 isSuccess -property user.email
 git config --global http.proxy http://165.225.104.34:9480
 isSuccess -property http.proxy
 git config --global https.proxy http://165.225.104.34:9480
 isSuccess -property https.proxy
-
-
-
-	
