b5236cf2e0ca1fabc79d5c0e18a224f47c78e98a
Sunil.Jacob <sunil1405@gmail.com>
2018-09-18 12:26:39 +0530

    Testing script - Appveyor

--- setup-scripts/test.ps1
+++ setup-scripts/test.ps1
@@ -1,7 +1,1 @@
-describe 'Test JAVA installation' {
-  $out=&"javac.exe" -version 2>&1
-  $version = $out[0].toString().SubString(6,3)
-  it 'should return 1.8 or 1.7 or 1.6' {
-    $version | should be 1.8 or 1.7 or 1.6
-  }
-}
+ls
