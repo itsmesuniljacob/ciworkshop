65025e3790abc245e6fffe72825fc7bc09ced74e
Sunil.Jacob <sunil1405@gmail.com>
2018-09-18 11:01:48 +0530

    Testing script

--- setup-scripts/test.ps1
+++ setup-scripts/test.ps1
@@ -1,1 +1,7 @@
-ls
+describe 'Test JAVA installation' {
+  $out=&"javac.exe" -version 2>&1
+  $version = $out[0].toString().SubString(6,3)
+  it 'should return 1.8 or 1.7 or 1.6' {
+    $version | should be 1.8 or 1.7 or 1.6
+  }
+}
