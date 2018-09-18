1dadcc8bdd8f1bbb56c31c7bc14b317bde2664cb
Sunil.Jacob <sunil1405@gmail.com>
2018-09-18 11:08:58 +0530

    Testing script

--- Install-BuildPrerequisite.ps1
+++ Install-BuildPrerequisite.ps1
@@ -1,3 +1,4 @@
+Set-PSRepository -Name 'PSGallery' -InstallationPolicy 'Trusted'
 Install-PackageProvider -Name "NuGet" -Force
 Install-Module -Name "PSScriptAnalyzer" -Force -SkipPublisherCheck
 Install-Module -Name "Pester" -Force -SkipPublisherCheck
