63015cb6bb54188f2090caa49de7a8017f4b79b9
Sunil.Jacob <sunil1405@gmail.com>
2018-10-03 11:47:37 +0530

    Initial code

---
+++ src/main/java/com/example/workshop/hello/HelloController.java
@@ -0,0 +1,23 @@
+package com.example.workshop.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+//    private FeatureManager featureManager;
+//
+//    public HelloController(FeatureManager featureManager) {
+//        this.featureManager = featureManager;
+//    }
+
+    @RequestMapping("/hello")
+    public String greetingPhilips() {
+
+        if(FeatureToggles.FEATURE_ONE.isActive()) {
+            return "Hello Philips";
+        }
+        return "Hello Philips Innovation Campus";
+    }
+}
